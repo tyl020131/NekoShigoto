5d1ae908d749177395947a8ae3840cf452193bff
WongZH <wongzh-pm20@student.tarc.edu.my>
2023-04-16 16:49:02 +0800

    k

---
+++ app/src/main/java/com/example/nekoshigoto/Applicant.kt
@@ -0,0 +1,5 @@
+package com.example.nekoshigoto
+
+data class Applicant(val name:String, val age:Int, val image:Int){
+
+}
---
+++ app/src/main/java/com/example/nekoshigoto/VacancyDetail.kt
@@ -0,0 +1,16 @@
+package com.example.nekoshigoto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class VacancyDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vacancy_detail)
+    }
+
+}
