00c219f1aab217bb4732bfd0f6b29e7eba577b4b
tankc2002 <tankc-pm20@student.tarc.edu.my>
2023-04-20 19:40:57 +0800

    change name of the submit application file

--- app/src/main/java/com/example/nekoshigoto/SubmitApplicationFragment.kt
+++ app/src/main/java/com/example/nekoshigoto/SubmitApplicationFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class SubmitApplication : Fragment() {
+class SubmitApplicationFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
