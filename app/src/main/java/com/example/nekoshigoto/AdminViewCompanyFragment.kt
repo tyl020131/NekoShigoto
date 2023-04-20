f1cf557d93142ac589aa4cb3b1090cb5bbb8b647
WongZH <wongzh-pm20@student.tarc.edu.my>
2023-04-20 23:50:19 +0800

    Admin User view siapbale

--- app/src/main/java/com/example/nekoshigoto/AdminViewCompanyFragment.kt
+++ app/src/main/java/com/example/nekoshigoto/AdminViewCompanyFragment.kt
@@ -9,10 +9,18 @@ import androidx.databinding.DataBindingUtil
 import com.example.nekoshigoto.databinding.FragmentAdminViewCompanyBinding
 
 class AdminViewCompanyFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val binding = DataBindingUtil.inflate<FragmentAdminViewCompanyBinding>(inflater,
-            R.layout.fragment_admin_view_company,container,false)
+    private lateinit var binding : FragmentAdminViewCompanyBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = FragmentAdminViewCompanyBinding.inflate(inflater, container, false)
+
+
+
         return binding.root
+
     }
 }
